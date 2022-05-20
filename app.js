eb13bc48a976221e2dd53d8d452a781db7411cad
SteffanCrowley <steffancrow@gmail.com>
2022-05-20 16:00:14 -0700

    Created win condition method

--- app.js
+++ app.js
@@ -3,15 +3,17 @@ const gameBoard = (() => {
   const container = document.querySelector(".container");
 
   //iterates through array populating the board
-  const makeBoard = (board) => {
+  const makeBoard = () => {
     for (i = 0; i < 9; i++) {
       const card = document.createElement("div");
       card.classList.add("card");
       card.id = i;
       container.append(card);
-      card.innerHTML = `${board[i]}`;
     }
   };
+
+  makeBoard(); //creates initial board
+
   return {
     makeBoard,
   };
@@ -22,13 +24,32 @@ const gameBoard = (() => {
 const playGame = (() => {
   let board = ["", "", "", "", "", "", "", "", ""];
 
-  gameBoard.makeBoard(board); //creates initial board
-
   //updates array index based on click, clears board,
   //remakes it with new array index
   function placeMark(e) {
-    board[this.id] = "X";
+    board[this.id] = 1;
+    winCondition(board);
     document.getElementById(`${this.id}`).innerHTML = "O";
+  }
+
+  function winCondition(board) {
+    if (board[0] + board[1] + board[2] == 3) {
+      console.log("win");
+    } else if (board[3] + board[4] + board[5] == 3) {
+      console.log("win");
+    } else if (board[6] + board[7] + board[8] == 3) {
+      console.log("win");
+    } else if (board[0] + board[3] + board[6] == 3) {
+      console.log("win");
+    } else if (board[1] + board[4] + board[7] == 3) {
+      console.log("win");
+    } else if (board[2] + board[5] + board[8] == 3) {
+      console.log("win");
+    } else if (board[0] + board[4] + board[8] == 3) {
+      console.log("win");
+    } else if (board[2] + board[4] + board[6] == 3) {
+      console.log("win");
+    }
   }
 
   //listens for clicks on the cards
