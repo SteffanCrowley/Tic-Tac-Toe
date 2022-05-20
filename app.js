b0b2424e3148345486a74c077a41188d8dca9ea7
SteffanCrowley <steffancrow@gmail.com>
2022-05-20 07:56:22 -0700

    Create JS module that makes board and populates

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-const gameBoard = (() => {})();
+const gameBoard = (() => {
+  const container = document.querySelector(".container");
+  let board = ["X", "O", "X", "X", "O", "X", "X", "O", "X"];
+
+  for (i = 0; i < 9; i++) {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    container.append(card);
+    card.innerHTML = `${board[i]}`;
+  }
+})();
+
+// const divs = document.querySelectorAll("card");
+// divs.forEach((card) => card.addEventListener("click", placeMark));
